f2549c15a85d4123ef76f2b39d2eb4d2ed2ca592
Shahulog <naretame@gmail.com>
2024-04-18 20:28:26 +0900

    aiueo

---
+++ SampleD2DEffect.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Controls;
+using YukkuriMovieMaker.Exo;
+using YukkuriMovieMaker.Player.Video;
+using YukkuriMovieMaker.Plugin.Effects;
+
+namespace BlinkinYMM4Plugin.SampleD2DEffect
+{
+    /// <summary>
+    /// 映像エフェクト
+    /// 映像エフェクトには必ず[VideoEffect]属性を設定してください。
+    /// </summary>
+    [VideoEffect("点滅しながら登場退場", new[] { "登場退場" }, new string[] { })]
+    internal class SampleD2DVideoEffect : VideoEffectBase
+    {
+        /// <summary>
+        /// エフェクトの名前
+        /// </summary>
+        public override string Label => "点滅しながら登場退場";
+
+        [Display(GroupName = "登場退場", Name = "登場", Description = "アイテムが登場する際にエフェクトを適用する")]
+        [ToggleSlider]
+        public bool IsAppearance { get => isAppearance; set => Set(ref isAppearance, value); }
+        bool isAppearance = false;
+
+        [Display(GroupName = "登場退場", Name = "退場", Description = "アイテムが退場する際にエフェクトを適用する")]
+        [ToggleSlider]
+        public bool IsExit { get => isExit; set => Set(ref isExit, value); }
+        bool isExit = false;
+
+        [Display(GroupName = "登場退場", Name = "効果時間", Description = "エフェクトが再生される秒数")]
+        [TextBoxSlider("F2", "秒", 0.00, 1.00)]
+        [DefaultValue(0.30)]
+        [Range(0.00, 100.00)]
+        public double Duration { get => duration; set => Set(ref duration, value); }
+        double duration = 0.00;
+
+        [Display(GroupName = "登場退場", Name = "表示間隔", Description = "点滅する間隔")]
+        [AnimationSlider("F2", "秒", 0.00, 1.00)]
+        public Animation Interval { get; } = new Animation(0.30, 0.00, 100.00);
+
+        [Display(GroupName = "登場退場", Name = "フレーム指定", Description = "カンマ区切りで透明にしたいフレームを指定します(例：2,6->2と6フレームで透明)")]
+        [TextEditor(AcceptsReturn = true)]
+        public string FrameSelectText { get => frameSelectText; set => Set(ref frameSelectText, value); }
+        string frameSelectText = string.Empty;
+
+        [Display(GroupName = "登場退場", Name = "反転", Description = "表示状態と透明状態を入れ替えます")]
+        [ToggleSlider]
+        public bool BlinkinToggle { get => blinkintoggle; set => Set(ref blinkintoggle, value); }
+        bool blinkintoggle = false;
+
+        /*
+        [Display(GroupName = "登場退場", Name = "デバッグ", Description = "カンマ区切りで透明にしたいフレームを指定します(例：2,6->2と6フレームで透明)")]
+        [TextEditor(AcceptsReturn = true)]
+        public string Text { get => text; set => Set(ref text, value); }
+        string text = string.Empty;
+        */
+
+        /// <summary>
+        /// ExoFilterを作成する
+        /// </summary>
+        /// <param name="keyFrameIndex">キーフレーム番号</param>
+        /// <param name="exoOutputDescription">exo出力に必要な各種パラメーター</param>
+        /// <returns></returns>
+        public override IEnumerable<string> CreateExoVideoFilters(int keyFrameIndex, ExoOutputDescription exoOutputDescription)
+        {
+            var fps = exoOutputDescription.VideoInfo.FPS;
+            return new[]
+            {
+                $"_name=ぼかし\r\n" +
+                $"_disable={(IsEnabled ?1:0)}\r\n" +
+                $"縦横比=0.0\r\n" +
+                $"光の強さ=0\r\n" +
+                $"サイズ固定=0\r\n",
+            };
+        }
+
+        /// <summary>
+        /// 映像エフェクトを作成する
+        /// </summary>
+        /// <param name="devices">デバイス</param>
+        /// <returns>映像エフェクト</returns>
+        public override IVideoEffectProcessor CreateVideoEffect(IGraphicsDevicesAndContext devices)
+        {
+            return new SampleD2DVideoEffectProcessor(devices, this);
+        }
+
+        /// <summary>
+        /// クラス内のIAnimatableの一覧を取得する
+        /// </summary>
+        /// <returns></returns>
+        protected override IEnumerable<IAnimatable> GetAnimatables() => new IAnimatable[] { Interval };
+    }
+}
---
+++ SampleD2DVideoEffectProcessor.cs
@@ -0,0 +1,162 @@
+﻿using BlinkinYMM4Plugin.SampleD2DEffect;
+using Vortice.Direct2D1;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Player.Video;
+using System.Text.RegularExpressions;
+using Windows.Win32.UI.KeyboardAndMouseInput;
+using Windows.Security.Cryptography.Core;
+namespace BlinkinYMM4Plugin.SampleD2DEffect
+{
+    internal class SampleD2DVideoEffectProcessor : IVideoEffectProcessor
+    {
+        readonly SampleD2DVideoEffect item;
+        readonly Vortice.Direct2D1.Effects.Opacity opacityEffect;
+
+        public ID2D1Image Output { get; }
+
+        double duration;
+        double interval;
+        string frameSelectText;
+        bool blinkintoggle;
+        bool isAppearance;
+        bool isExit;
+
+        public SampleD2DVideoEffectProcessor(IGraphicsDevicesAndContext devices, SampleD2DVideoEffect item)
+        {
+            this.item = item;
+
+            opacityEffect = new Vortice.Direct2D1.Effects.Opacity(devices.DeviceContext);
+            Output = opacityEffect.Output;//EffectからgetしたOutputは必ずDisposeする。Effect側ではDisposeされない。
+        }
+
+        /// <summary>
+        /// エフェクトに入力する映像を設定する
+        /// </summary>
+        /// <param name="input"></param>
+        public void SetInput(ID2D1Image input)
+        {
+            opacityEffect.SetInput(0, input, true);
+        }
+
+        /// <summary>
+        /// エフェクトに入力する映像をクリアする
+        /// </summary>
+        public void ClearInput()
+        {
+            opacityEffect.SetInput(0, null, true);
+        }
+
+        /// <summary>
+        /// エフェクトを更新する
+        /// </summary>
+        /// <param name="effectDescription">エフェクトの描画に必要な各種設定項目</param>
+        /// <returns>描画関連の設定項目</returns>
+        public DrawDescription Update(EffectDescription effectDescription)
+        {
+            var frame = effectDescription.ItemPosition.Frame;
+            var length = effectDescription.ItemDuration.Frame;
+            var fps = effectDescription.FPS;
+            double interval = item.Interval.GetValue(frame, length, fps);
+
+
+            bool isDisplay = true;
+            
+           
+            double duration = item.Duration;
+            float durationByFrame = (float)duration * (float)fps;
+            bool blinkintoggle = item.BlinkinToggle;
+            float visible = (float)1.0;
+            float hidden = (float)0.0;
+            bool isAppearance = item.IsAppearance;
+            bool isExit = item.IsExit;
+
+            //item.Text = $"frame:{frame}\nlength:{length}\nfps:{fps}\nDuration frame:{durationByFrame}";
+
+            int[] frameSelects;
+
+            try
+            {
+                frameSelects = Regex.Replace(item.FrameSelectText, @"[^\d,]", "").Split(',').Select(x => int.Parse(x)).ToArray();
+            }
+            catch
+            {
+
+                frameSelects = [];
+            }
+
+            void Blinkin(int frame)
+            {
+               
+                var seconds = (float)frame / (float)fps;
+                if (seconds < duration)
+                {
+                    if (((seconds < interval) || ((int)Math.Floor(seconds / interval) % 2 == 0)))
+                    {
+                        //item.Text = $"{seconds.ToString()},{interval.ToString()}";
+                        isDisplay = true;
+                    }
+                    else
+                    {
+                        isDisplay = false;
+                    }
+                }
+
+
+                if (frameSelects.Contains(frame))
+                {
+                    isDisplay = false;
+                }
+
+                if (isDisplay)
+                {
+                    opacityEffect.Value = blinkintoggle ? hidden : visible;
+                }
+                else
+                {
+                    opacityEffect.Value = blinkintoggle ? visible : hidden;
+                }
+
+               
+            }
+
+
+            int exitStarFrame = length - (int)durationByFrame;
+            //item.Text += $"\nExit Start Frame:{exitStarFrame}";
+            if (frame < (int)durationByFrame)
+            {
+                if (isAppearance)
+                {
+                    Blinkin(frame);
+                }
+            }
+            else if (frame > exitStarFrame)
+            {
+                if (isExit)
+                {
+                    int exitFrame =   frame - exitStarFrame;
+                   // item.Text += $"\nExit Frame:{exitFrame}";
+                    Blinkin(exitFrame);
+                }
+            }
+            else
+            {
+                opacityEffect.Value = visible;
+            }
+
+
+            
+            this.duration = duration;
+            this.interval = interval;
+            this.blinkintoggle = blinkintoggle;
+            return effectDescription.DrawDescription;
+        }
+
+
+        public void Dispose()
+        {
+            opacityEffect.SetInput(0, null, true);//Inputは必ずnullに戻す。
+            Output.Dispose();//EffectからgetしたOutputは必ずDisposeする。Effect側ではDisposeされない。
+            opacityEffect.Dispose();
+        }
+    }
+}
